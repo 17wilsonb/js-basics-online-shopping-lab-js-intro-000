fd520f369f137698bff6087dea41c050cc4480ba
Brett Wilson <wilson.3458@osu.edu>
2020-05-14 03:08:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var cart = [];
 
-function getCart() {
+function cart() {
  return cart;
 }
 
-function setCart(c) {
+function cart(c) {
   cart = c;
   return cart;
 }
@@ -15,44 +15,44 @@ function convertToItem(itemName){
 }
 
 function addToCart(item) {
-  getCart.push(convertToItem(item));
+  cart.push(convertToItem(item));
   return `${item.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  if (getCart().length === 0){
+  if (cart().length === 0){
     return "Your shopping cart is empty.";
   }
   var status = 'In your cart, you have ';
-  if ( getCart().length >= 1 ) {
-    status += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`;
+  if ( cart().length >= 1 ) {
+    status += `${cart()[0].itemName} at $${cart()[0].itemPrice}`;
   }
-  if ( getCart().length >= 2 ) {
+  if ( cart().length >= 2 ) {
     var middle = '';
-    for (var i = 1; i < getCart().length - 1; i++) {
-      middle += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`;
+    for (var i = 1; i < cart().length - 1; i++) {
+      middle += `, ${cart()[i].itemName} at $${cart()[i].itemPrice}`;
     }
-    status += `${middle}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}`;
+    status += `${middle}, and ${cart()[cart().length - 1].itemName} at $${cart()[cart().length - 1].itemPrice}`;
   }
   return `${cartDescription}.`;
 }
 
 function total() {
   var sum = 0;
-  for (var i = 0; i < getCart().length; i++) {
-    sum += getCart()[i].itemPrice;
+  for (var i = 0; i < cart().length; i++) {
+    sum += cart()[i].itemPrice;
   }
   return sum;
 }
 
 function removeFromCart(item) {
   var searchResult;
-  for (var i = 0; i < getCart().length; i++) {
-    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  for (var i = 0; i < cart().length; i++) {
+    if (cart()[i].itemName === itemName) {searchResult = cart()[i]}
   }
   if(searchResult){
     var indexOfItemToRemove = cart.indexOf(searchResult);
-    return getCart().splice(indexOfItemToRemove,1);
+    return cart().splice(indexOfItemToRemove,1);
   } else {
     return "That item is not in your cart.";
   }
@@ -63,7 +63,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var totalPrice = total();
-    setCart([]);
+    cart([]);
     return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`;
   }
 }
